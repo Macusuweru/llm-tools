96037dd05d7fd641af651558bcb5010a334969c2
Maxwell <macusuweru@gmail.com>
2025-01-20 14:36:01 -0700

    Test.py was irrelevant

--- test.py
+++
@@ -1,7 +0,0 @@
-tags = {
-    "tag1": {"tag1.1": ["tag1.1.1", "tag1.1.2"],
-             "tag1.2": ["tag1.2.1"]},
-    "tag2": {"tag2.1": ["tag2.1.1"]}
-}
-for tag in tags:
-    print(tag == "tag1")
